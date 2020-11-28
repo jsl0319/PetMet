2392f19519aceea3f7f88093f87f2b56188f9660
Hyun <iamhyunji_kim@icloud.com>
2020-11-28 15:29:33 +0900

    현지 : User Servlet 기초

--- src/com/petmet/web/entity/User.java
+++ src/com/petmet/web/entity/User.java
@@ -1,41 +1,42 @@
 package com.petmet.web.entity;
 
 public class User {
-
-	public int	id;
+	// ------------------------ Attribute ------------------------
+	private int id;
+	private String name;
 	private String uid;
 	private String pwd;
-	private String name;
 	private String birthday;
-
-
-public User() {
-//	this(0,null,null,null,null);
-}
 	
-	public User(int id, String uid, String pwd, String name, String birthday) {
+	// ------------------------ Constructor ------------------------
+	public User() {
+		this(0, null, null, null, null);
+	}
+
+	public User(int id, String name, String uid, String pwd, String birthday) {
 		super();
 		this.id = id;
+		this.name = name;
 		this.uid = uid;
 		this.pwd = pwd;
-		this.name = name;
 		this.birthday = birthday;
 	}
-
-
-
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", uid=" + uid + ", pwd=" + pwd + ", name=" + name + ", birthday=" + birthday + "]";
-	}
-
-
+	
+	// ------------------------ Getters/Setters ------------------------
 	public int getId() {
 		return id;
 	}
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getUid() {
@@ -54,14 +55,6 @@ public User() {
 		this.pwd = pwd;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public String getBirthday() {
 		return birthday;
 	}
@@ -70,5 +63,10 @@ public User() {
 		this.birthday = birthday;
 	}
 
+	// ------------------------ Functions ------------------------
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", uid=" + uid + ", pwd=" + pwd + ", birthday=" + birthday + "]";
+	}
+	
 }
-
