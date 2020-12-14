b288a29d8f9bc1650a4371ef6910c7d1b420133e
aengun <aengun@icloud.com>
2020-12-15 02:19:22 +0900

    건영 : petplace -> list, detail jsp 완성

--- src/com/petmet/web/controller/admin/petPlace/RegController.java
+++ src/com/petmet/web/controller/admin/petPlace/RegController.java
@@ -1,6 +1,7 @@
 package com.petmet.web.controller.admin.petPlace;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,32 +10,49 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.petmet.web.entity.PetPlace;
+import com.petmet.web.entity.PetPlaceCategory;
+import com.petmet.web.service.PetPlaceCategoryService;
 import com.petmet.web.service.PetPlaceService;
 
 @WebServlet("/admin/petplace/reg")
 public class RegController extends HttpServlet{
 	
+//	@Override
+//	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		
+//		if(request.getMethod().equals("GET"))
+//			request.getRequestDispatcher("reg.jsp").forward(request, response);
+//		else if(request.getMethod().equals("POST")) {
+//			
+//			String categoryId = request.getParameter("categoryId");
+//			String name = request.getParameter("name");
+//			String address = request.getParameter("address");
+//			String homepage = request.getParameter("homepage");
+//			String phone = request.getParameter("phone");
+//			String location = request.getParameter("location");
+//			String content = request.getParameter("content");
+//			String files = request.getParameter("files");
+//			PetPlace pp = new PetPlace(categoryId, name, address, homepage, phone, location, content, files);
+//			
+//			PetPlaceService service = new PetPlaceService();
+//			service.insert(pp);
+//			
+//			response.sendRedirect("list");
+//		}
+//	}
+	
 	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		PetPlaceCategoryService service = new PetPlaceCategoryService();
+		List<PetPlaceCategory> list = service.getList();
 		
-		if(request.getMethod().equals("GET"))
-			request.getRequestDispatcher("reg.jsp").forward(request, response);
-		else if(request.getMethod().equals("POST")) {
-			
-			String categoryId = request.getParameter("categoryId");
-			String name = request.getParameter("name");
-			String address = request.getParameter("address");
-			String homepage = request.getParameter("homepage");
-			String phone = request.getParameter("phone");
-			String location = request.getParameter("location");
-			String content = request.getParameter("content");
-			String files = request.getParameter("files");
-			PetPlace pp = new PetPlace(categoryId, name, address, homepage, phone, location, content, files);
-			
-			PetPlaceService service = new PetPlaceService();
-			service.insert(pp);
-			
-			response.sendRedirect("list");
-		}
+		request.setAttribute("list", list);
+		
+		request.getRequestDispatcher("/admin/petplace/reg.jsp").forward(request, response);
+
 	}
+	
+	
 }
