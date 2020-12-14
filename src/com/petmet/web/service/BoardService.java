4e98c3ba5b6c696cbd2094f858057bfd39af02d8
Hyun <iamhyunji_kim@icloud.com>
2020-12-14 20:35:08 +0900

    현지 : DAO / Service
    	- BoardService
    	- BoardDao
    	- JDBCBoardDao

--- src/com/petmet/web/service/BoardService.java
+++ src/com/petmet/web/service/BoardService.java
@@ -16,14 +16,64 @@ public class BoardService {
 	}
 	
 	//---------------------------------- Functions ----------------------------------
+	public int insert(Board board) {
+		return boardDao.insert(board);
+	}
+	
+	public int update(Board board) {
+		return boardDao.update(board);
+	}
+	
+	public int delete(int id) {
+		return boardDao.delete(id);
+	}
+	
+	public Board get(int id) {
+		return boardDao.get(id);
+	}
+	
+	// -------------- getList --------------
 	public List<Board> getList() {
-		
 		return boardDao.getList();
 	}
 	
-	public int deleteList(int[] ids) {
-		for(int i=0; i < ids.length; i++)
-			
-		return 0;
+	public List<Board> getList(int page){
+		return getList(1, 20);
 	}
+	
+	public List<Board> getList(int page, int size){
+		int startIndex = 1 + (page - 1) * size;
+		int endIndex = size * page;
+		
+		return boardDao.getList(startIndex, endIndex);
+	}
+	
+	public List<Board> getList(String searchOption, String searchText){
+		return null;
+	}
+	
+	public List<Board> getList()
+	// ---------------------------------------
+	public int deleteList(int[] ids) {
+		int result = 0;
+		
+		for(int i=0; i < ids.length; i++, result++) {
+			int id = ids[i];
+			boardDao.delete(id);
+		}
+		
+		return result;
+	}
+	
+	public int updateList(List<Board> list) {
+		int result = 0;
+		
+		for(Board board : list) {
+			boardDao.update(board);
+			result++;
+		}
+		
+		return result;
+	}
+	
 }
