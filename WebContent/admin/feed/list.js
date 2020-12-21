eedb68e3c9a949a59a29ab0016b88c25f0f97b84
jsl0319 <kokonb0@gmail.com>
2020-12-21 20:02:34 +0900

    지선 : Ajax.....

--- WebContent/admin/feed/list.js
+++ WebContent/admin/feed/list.js
@@ -3,9 +3,8 @@ window.addEventListener("load", function() {
 	var section = main.querySelector(".main-section");
 	var tbody = section.querySelector("tbody");
 	var pager = section.querySelector(".pager");
+	var button1 = section.querySelector(".button1");
 	
-	// 1페이지 로드
-	load();
 	
 	pager.onclick = function(event){
 		event.preventDefault();
@@ -14,10 +13,16 @@ window.addEventListener("load", function() {
 		load(pager);
 	}
 	
+	window.onload = function(){
+	        load();
+	}
+	/*button1.onclick = function(){ */  
+	    /*}*/
+
 	function load(page){
 		
 		//------페이지 초기화
-		if(page==undefined)
+		if(page == undefined)
 			page = 1;
 		
 		//-------요청 객체 생성
@@ -31,7 +36,7 @@ window.addEventListener("load", function() {
 			var views = JSON.parse(request.responseText);
 			
 			//-----------결과값 담을 태그와 테디터를 데이터 수만큼 생성
-			for(var i =0; i<views.length; i++){
+			for(var i = 0; i<views.length; i++){
 				var v = views[i];
 				
 				var tr = '<tr> \
