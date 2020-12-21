8ca4373f5a9338cbe367dd3590c78878d4e22f88
aengun <aengun@icloud.com>
2020-12-21 15:25:23 +0900

    건영 : js 경로 변경

--- WebContent/admin/petplace/detail.js
+++ WebContent/admin/petplace/detail.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function() {
 
-	var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+	/*var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
 		mapOption = {
 			center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
 			level: 3 // 지도의 확대 레벨
@@ -13,6 +13,14 @@ window.addEventListener("load", function() {
 		position: map.getCenter()
 	});
 
-	marker.setMap(map);
+	marker.setMap(map);*/
 
+	function searchParam(key) {
+		return new URLSearchParams(location.search).get(key);
+	};
+
+	var id = searchParam('id');
+
+	console.log(id);
+	
 });
